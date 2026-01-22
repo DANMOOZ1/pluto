ceb00e75707060e89a9a15d55893ae436bcd8d51
UYEOL32 <uyeoli248@gmail.com>
2026-01-23 00:22:44 +0900

    Luna HTA fix

--- Assets/Scripts/Unit/HeatAreaRules/LunaHeatAreaRule.cs
+++ Assets/Scripts/Unit/HeatAreaRules/LunaHeatAreaRule.cs
@@ -6,23 +6,15 @@ public class LunaHeatAreaRule : HeatAreaRule
 {
     public override List<Vector3Int> HeatAreaRuleFunc(Vector3Int from, Vector3Int to)
     {
-        int dx = Mathf.Abs(to.x - from.x);
-        int dy = Mathf.Abs(to.y - from.y);
+        int dx = to.x - from.x;
+        int dy = to.y - from.y;
 
-        if (dx > dy)
-        {
-            Vector3Int v = new Vector3Int(1, 0, 0);
-            return new List<Vector3Int>() { to, to + v*(dx/(to.x - from.x)) };
-        }else if (dy > dx)
-        {
-            Vector3Int v = new Vector3Int(0, 1, 0);
-            return new List<Vector3Int>() { to, to + v*(dy/(to.y - from.y)) };
-        }
-        else
-        {
-            Debug.LogWarning("HeatArea 계산 오류 발생");
-            return new List<Vector3Int>() { to };
-        }
+        if(dy == 0) return new List<Vector3Int>() { from + new Vector3Int(dx/Mathf.Abs(dx), 0, 0), from + new Vector3Int(2*(dx/Mathf.Abs(dx)), 0, 0)};
+        if (dx == 0) return new List<Vector3Int>() { from + new Vector3Int(0, dy/Mathf.Abs(dy) , 0), from + new Vector3Int(0, 2*(dy/Mathf.Abs(dy)) , 0)};
+        
+        Debug.LogWarning("HeatArea 계산 오류 발생");
+        return new List<Vector3Int>() { to };
+        
     }
 }
 
